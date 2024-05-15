3b2a9f0dd0121b067aefe3f284a657f43ae87dbd
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-05-15 18:30:28 +0000

    asve

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames.al-sharoa
+// Created on: may 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
